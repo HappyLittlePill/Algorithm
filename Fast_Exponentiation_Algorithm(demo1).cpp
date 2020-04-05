4bbccca00e734127133bb290faadda7e322f2578
HapplyLittlePill <851621083@qq.com>
2020-04-05 13:22:17 +0800

    add 两个方法快速幂(快速幂取模)

---
+++ Fast_Exponentiation_Algorithm(demo1).cpp
@@ -0,0 +1,24 @@
+//递归快速幂（对大素数取模）
+#include <stdio.h>
+#define MOD 3337
+typedef long long ll;
+ll qpow(ll a, ll n)
+{
+    if (n == 0)
+        return 1;
+    else if (n % 2 == 1)
+        return qpow(a, n - 1) * a % MOD;
+    else
+    {
+        ll temp = qpow(a, n / 2) % MOD;
+        return temp * temp % MOD;
+    }
+}
+int main(){
+    ll a,b;
+    printf("请输入底数a，指数b：\n");
+    scanf("%lld%lld",&a,&b);
+    ll out = qpow(a, b);
+    printf("%lld\n",out);
+    return 0;
+}
