7c690246395cd93db1e055f14cee7168b2719ddf
Volcano <851621083@qq.com>
2020-04-11 11:52:31 +0800

    update 递归快速幂取模（对大素数取模）

--- Fast_exponentiation_algorithm.cpp
+++ Fast_exponentiation_algorithm.cpp
@@ -1,26 +1,27 @@
 // 递归快速幂取模（对大素数取模）
 // 转载于https://zhuanlan.zhihu.com/p/95902286
 // 原则是步步取模，如果MOD较大，还应当开long long
+// 原理是在递归快速幂算法(Recursively_fast_powers)的基础上增加取模运算，核心思想还是递归，以空间换时间，效率不如蒙哥马利算法
 #include <stdio.h>
-#define MOD 3337
 typedef long long ll;
-ll qpow(ll a, ll n)
+ll qpow(ll a, ll n, ll MOD)
 {
     if (n == 0)
         return 1;
     else if (n % 2 == 1)
-        return qpow(a, n - 1) * a % MOD;
+        return qpow(a, n - 1, MOD) * a % MOD;
     else
     {
-        ll temp = qpow(a, n / 2) % MOD;
+        ll temp = qpow(a, n / 2, MOD) % MOD;
         return temp * temp % MOD;
     }
 }
-int main(){
-    ll a,b;
-    printf("请输入底数a，指数b：\n");
-    scanf("%lld%lld",&a,&b);
-    ll out = qpow(a, b);
-    printf("%lld\n",out);
+int main()
+{
+    ll a, b, MOD;
+    printf("请输入底数a：，指数b：，模c:\n");
+    scanf("%lld%lld%lld", &a, &b, &MOD);
+    ll out = qpow(a, b, MOD);
+    printf("%lld\n", out);
     return 0;
 }
